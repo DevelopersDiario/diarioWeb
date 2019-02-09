7853b63d1cb52674a22a10a69a7238b12b7c8a2c
benya <benjaminlopezluis@gmail.com>
2019-02-09 01:19:54 -0600

    Se agrega usuario admin de prueba

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,24 @@
+import seguridad.Rol
+import seguridad.Usuario
+import seguridad.UsuarioRol
+
 class BootStrap {
 
     def init = { servletContext ->
+//       def rol = Rol.findByAuthority('ROLE_ADMIN')?: new Rol(authority: 'ROLE_ADMIN').save(flush: true)
+//        def adminRole = new Rol(authority: 'ROLE_ADMIN').save(flush: true)
+//        def userRole = new Rol(authority: 'ROLE_ALUMNO').save(flush: true)
+//        def colaboradorRole = new Rol(authority: 'ROLE_COLABORADOR').save(flush: true)
+//
+/*        def testUser = new Usuario(username: 'benja', password: 'password')
+        testUser.save(flush: true)
+
+        UsuarioRol.create testUser, rol, true
+
+        assert Usuario.count() == 1
+        assert Rol.count() == 1
+        assert UsuarioRol.count() == 1
+        */
     }
     def destroy = {
     }
